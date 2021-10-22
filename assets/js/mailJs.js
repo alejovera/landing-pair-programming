25629afdc3a6aa4f2a4dcf0ebfce0e76145a85e9
Alejo Vera <vera.alejo0903@gmail.com>
2021-10-22 20:22:46 -0300

    mailJs implementation

---
+++ assets/js/mailJs.js
@@ -0,0 +1,14 @@
+function sendMail(params) {
+    event.preventDefault()
+    var tempParams = {
+        from_name: document.getElementById('from_name').value,
+        to_name: document.getElementById('email_user').value,
+        company_name: document.getElementById('company_name').value,
+        title_message: document.getElementById('title_message').value,
+        message: document.getElementById('message').value,
+    };
+    emailjs.send('contact_service_landing', 'template_6t1ma4k', tempParams)
+        .then(function(res) {
+            console.log(res.status);
+        })
+}
